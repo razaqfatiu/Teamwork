9f0d71e6157fd22d7b538731514d8a822c52b397
Fatiu Isiaka <fatiuisiaka@Fatius-MacBook-Pro.local>
2019-11-14 22:45:04 +0100

    feature(employee-signin):create employee signin endpoint

--- backend/api/controllers/employee.js
+++ backend/api/controllers/employee.js
@@ -1,6 +1,56 @@
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { pool } = require('../db/config');
+
+
 module.exports = {
   getEmployees(req, res) {
     res.status(200);
     res.json({ employees: 'employees' });
+  },
+  employeeSignIn(req, res) {
+    const { email, password } = req.body;
+    const query = {
+      text: 'SELECT * FROM employee where email = $1',
+      values: [email],
+    };
+
+    return pool.query(query).then((employee) => {
+      const { rows } = employee;
+      if (rows.length === 0) {
+        return res.status(401).json({
+          error: new Error('Employee not found'),
+        });
+      }
+      return bcrypt.compare(password, rows[0].password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({
+              error: new Error('Incorrect email or password'),
+            });
+          }
+          const token = jwt.sign(
+            { employeeId: rows[0].id },
+            process.env.EMPLOYEE_TOKEN_SECRET,
+            { expiresIn: '1h' },
+          );
+          return res.status(200).json({
+            employeeId: rows[0].id,
+            token,
+          });
+        })
+        .catch((error) => {
+          res.status(500).json({
+            error,
+          });
+        });
+    })
+      .catch((error) => {
+        res.status(500).json({
+          error,
+        });
+      });
   },
 };
