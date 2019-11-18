7dc52cba1eec3b4062236e0ca1bf49a9a70d043b
Fatiu Isiaka <fatiuisiaka@Fatius-MacBook-Pro.local>
2019-11-18 21:53:27 +0100

    feature(employee-article):create employee can get article endpoint

--- backend/api/controllers/article.js
+++ backend/api/controllers/article.js
@@ -21,7 +21,6 @@ module.exports = {
       });
     })
       .catch((error) => {
-        console.log(error);
         res.status(400).json({ error });
       });
   },
@@ -61,5 +60,31 @@ module.exports = {
       res.status(200).json({ message: 'Article Successfully deleted' });
     })
       .catch((error) => { res.status(400).json({ error }); });
+  },
+  getOneArticle(req, res) {
+    const { articleId } = req.params;
+    const selectArticle = {
+      name: 'selectArticle',
+      text: 'SELECT * FROM article WHERE id=$1',
+      values: [articleId],
+    };
+    const selectComment = {
+      name: 'selectComment',
+      text: 'SELECT id as "commentId", comment, employeeid as "authorId" FROM comment WHERE articleid=$1',
+      values: [articleId],
+    };
+
+    pool.query(selectComment).then((response) => pool.query(selectArticle).then((result) => {
+      const { rows } = result;
+      res.status(200).json({
+        id: rows[0].id,
+        createdOn: rows[0].created_at,
+        title: rows[0].id,
+        article: rows[0].article,
+        comments: response.rows,
+      });
+    })
+      .catch((error) => res.status(400).json({ error })))
+      .catch((error) => res.status(400).json({ error }));
   },
 };
