e1b3e97900b8731635547e606a740fcd5159ed59
Fatiu Isiaka <fatiuisiaka@Fatius-MacBook-Pro.local>
2019-11-20 23:28:11 +0100

    feature(employee-feed):create employee can get feed

---
+++ backend/api/controllers/feed.js
@@ -0,0 +1,16 @@
+const { pool } = require('../db/config');
+
+module.exports = {
+  getFeeds(req, res) {
+    const getFeed = {
+      name: 'getFeed',
+      text: `SELECT id, created_at as "createdOn", title, article as "article/url", employeeid as "authorId" FROM article 
+      UNION
+       SELECT id, created_at as "createdOn", title, imageurl as "url", employeeid as "authorId" FROM gif ORDER BY "createdOn" ASC`,
+    };
+    pool.query(getFeed).then((result) => {
+      res.json({ result });
+    })
+      .catch((error) => { console.log(error); res.status(400).json({ error }); });
+  },
+};
