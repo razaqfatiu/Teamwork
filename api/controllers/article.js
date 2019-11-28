a4317ec94d297773e0cbe914d61bf2267f29ab8a
Fatiu Isiaka <fatiuisiaka@Fatius-MacBook-Pro.local>
2019-11-28 13:25:02 +0100

    (ft-edit-article)refactor:create admin and owning users can edit article

--- api/controllers/article.js
+++ api/controllers/article.js
@@ -70,43 +70,4 @@ module.exports = {
         });
       });
   },
-  // deleteArticle(req, res) {
-  //   const { id } = req.params;
-  //   const deleteArticle = {
-  //     name: 'deleteArticle',
-  //     text: 'DELETE FROM article WHERE id = $1',
-  //     values: [id],
-  //   };
-  //   pool.query(deleteArticle).then(() => {
-  //     res.status(200).json({ message: 'Article Successfully deleted' });
-  //   })
-  //     .catch((error) => { res.status(400).json({ error }); });
-  // },
-  // getOneArticle(req, res) {
-  //   const { articleId } = req.params;
-  //   const selectArticle = {
-  //     name: 'selectArticle',
-  //     text: 'SELECT * FROM article WHERE id=$1',
-  //     values: [articleId],
-  //   };
-  //   const selectComment = {
-  //     name: 'selectComment',
-  //     text: `SELECT id as "commentId", comment,
-  // employeeid as "authorId" FROM comment WHERE articleid=$1`,
-  //     values: [articleId],
-  //   };
-
-  //   pool.query(selectComment).then((response) => pool.query(selectArticle).then((result) => {
-  //     const { rows } = result;
-  //     res.status(200).json({
-  //       id: rows[0].id,
-  //       createdOn: rows[0].created_at,
-  //       title: rows[0].title,
-  //       article: rows[0].article,
-  //       comments: response.rows,
-  //     });
-  //   })
-  //     .catch((error) => res.status(400).json({ error })))
-  //     .catch((error) => res.status(400).json({ error }));
-  // },
 };
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,13 +1,8 @@
 const express = require('express');
-// const { getAdmin, adminCreateUser } = require('../controllers/admin');
 const { userSignIn, adminCreateUser } = require('../controllers/user');
 const { userAuth } = require('../middleware/userAuth');
-// const { employeeAuth } = require('../middleware/employeeAuth');
 const { createArticle, editArticle } = require('../controllers/article');
-// const { createComment, createGifComment } = require('../controllers/comment');
-// const { getFeeds } = require('../controllers/feed');
-// const { createGif, getOneGif, deleteGif } = require('../controllers/gif');
-// const multer = require('../config/multerConfig');
+
 
 const userRouter = express.Router();
 
